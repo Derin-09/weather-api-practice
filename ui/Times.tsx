f03eafd7ee48f3cc21704d89e7517398210c6508
Derin <aderinsolasamashimi@gmail.com>
2025-08-16 15:59:47 +0100

    refactor: removed an unnecessary import

--- ui/Times.tsx
+++ ui/Times.tsx
@@ -1,7 +1,6 @@
 import { StaticImageData } from 'next/image'
 import Image from 'next/image'
 import React from 'react'
-import IconsMaps from '@/components/IconsMaps'
 
 interface Details {
   time: string
